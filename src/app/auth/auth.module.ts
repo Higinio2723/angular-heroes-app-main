edb72605c06e776a735b0bd86262c69df1c1202e
Higinio2723 <52228319+Higinio2723@users.noreply.github.com>
2023-07-05 18:03:13 -0600

    Se realiza la actualizacion para crear componentes

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material/material.module';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
@@ -17,6 +18,7 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
   imports: [
     CommonModule,
     AuthRoutingModule,
+    MaterialModule,
   ]
 })
 export class AuthModule { }
