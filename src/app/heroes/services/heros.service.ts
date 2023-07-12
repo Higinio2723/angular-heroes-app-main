1982378a538418061e879b20dc0704ed004394bb
Higinio2723 <52228319+Higinio2723@users.noreply.github.com>
2023-07-12 11:22:00 -0600

    Se realiza la busqueda del detalle para el heroe

--- src/app/heroes/services/heros.service.ts
+++ src/app/heroes/services/heros.service.ts
@@ -1,7 +1,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { enviroments } from 'src/environments/environments';
 
@@ -18,4 +18,10 @@ export class HeroesService {
         return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
     }
 
+    getHeroById(id: string): Observable<Hero | undefined>{
+        return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`).
+        pipe(
+            catchError(error => of(undefined) )
+        )
+    }
 }
