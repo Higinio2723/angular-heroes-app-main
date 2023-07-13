364f3afe51e8f6ce050618919f51c967000b717f
Higinio2723 <52228319+Higinio2723@users.noreply.github.com>
2023-07-13 11:45:12 -0600

    Se agregan las funcionalidades de editar y guardar superheroes

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heros.service';
 
 @Component({
   selector: 'app-new-page',
@@ -8,9 +11,51 @@ import { Component } from '@angular/core';
 })
 export class NewPageComponent {
 
+  constructor(private heroesService:HeroesService){}
+
+public heroForm = new FormGroup({
+  id: new FormControl<string>(''),              
+  superhero:    new FormControl<string>('', {nonNullable: true}),   
+  publisher:  new FormControl<Publisher>(Publisher.DCComics),     
+  alter_ego:      new FormControl(''), 
+  first_appearance:new FormControl(''),
+  characters:  new FormControl(''),    
+  alt_img:  new FormControl(''),   
+ }   
+)
+
 public publishers =[
   { id: 'DC Comics', desc:'DC - Comics'},
   { id: 'Marvel Comics', desc:'Marvel - Comics'},
 ] ;
+
+  get currentHero():Hero{
+    const hero = this.heroForm.value as Hero;
+    
+    return hero;
+  } 
+
+  
+ onSubmit():void{
+    console.log(
+     'formIsValid :'+ this.heroForm.valid,
+     this.heroForm.getRawValue()
+    )
+
+    if(this.heroForm.invalid) return;
+
+    if(this.currentHero.id){
+      this.heroesService.updateHero(this.currentHero)
+      .subscribe( hero => {
+         //TODO: mostrar snackbar
+      });
+      return;
+    }
+   
+     this.heroesService.addHero(this.currentHero)
+     .subscribe( hero => {
+       //TODO: mostrar snackbar, y navegar a /heroes/edit/ hero.id
+     })
+ }
 
 }
